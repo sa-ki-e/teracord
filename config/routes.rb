d3b63fe45a03e6c134081fbd261ed4324b573021
sa-ki-e <sakie0313@gmail.com>
2022-04-08 07:21:32 +0000

    [Add]devise:admin(ユーザ管理機能)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+    get 'users/edit'
+  end
  # 会員用
 # URL /users/sign_in ...
 devise_for :users, controllers: {
@@ -11,6 +15,11 @@ devise_for :users, controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+  root to: "users#index" #管理者側TOPページ
+  resources :users, only: [:index, :show, :edit, :update]
+end
 
 root to: "public/homes#top"
  end
